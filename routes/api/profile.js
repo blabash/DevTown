85fb3d8e9b89e39580bbe7c9b026066db739f2b8
brett <brett.labash@gmail.com>
2019-08-22 03:35:35 -0700

    Add DELETE route for posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -135,8 +135,6 @@ router.get('/user/:user_id', async (req, res) => {
       user: req.params.user_id
     }).populate('user', ['name', 'avatar']);
 
-    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
-
     res.json(profile);
   } catch (err) {
     console.error(err.message);
