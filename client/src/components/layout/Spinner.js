aae4b7ae386652dcb465d6106f38d0c67aa99767
brett <brett.labash@gmail.com>
2019-08-26 10:02:15 -0700

    Add Spinner component and profile availability checking to Dashboard component

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+  <i
+    className='fas fa-cog fa-spin'
+    style={{ fontSize: '72px', display: 'flex', justifyContent: 'center' }}
+    alt='loading'
+  ></i>
+);
