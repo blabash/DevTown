000f52892470bf11e38ce611309e7c7a6ab1fa37
brett <brett.labash@gmail.com>
2019-09-02 12:15:20 -0700

    Add like and unlike onClick handlers to PostItem component

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 export const getPosts = () => async dispatch => {
   try {
@@ -9,6 +9,40 @@ export const getPosts = () => async dispatch => {
     dispatch({
       type: GET_POSTS,
       payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Add like
+export const addLike = postId => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/${postId}/like`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data }
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Remove like
+export const removeLike = postId => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/${postId}/unlike`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data }
     });
   } catch (err) {
     dispatch({
